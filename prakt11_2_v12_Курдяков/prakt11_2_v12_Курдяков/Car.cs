1a0f073640356213cb443e573243827514669b23
Artyom812 <mega.kurdyakov@mail.ru>
2025-02-03 15:52:28 +0500

    Add files via upload

---
+++ prakt11_2_v12_Курдяков/prakt11_2_v12_Курдяков/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prakt11_2_v12_Курдяков
+{
+    public class Сar : Form1
+    {
+        private string brand;
+        private int cilinder;
+        private double power;
+        public Сar() //конструктор по умолчанию
+        {
+            brand = "Toyota";
+            cilinder = 0;
+            power = 0.0;
+        }
+        public Сar(string brand, int cilinder, double power) //конструктор с параметрами
+        {
+            this.brand = brand;
+            this.cilinder = cilinder;
+            this.power = power;
+        }
+        ~Сar() //деструктор
+        {
+            //освобождение ресурсов
+        }
+        public virtual string Sprg() //печать
+        {
+            return $"Марка: {brand}.\n Цилиндры: {cilinder}.\n Мощность: {power}.";
+        }
+        public void Brand(string newBrand)
+        {
+            brand = newBrand;
+        }
+    }
+}
---
+++ prakt11_2_v12_Курдяков/prakt11_2_v12_Курдяков/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prakt11_2_v12_Курдяков
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Сar сar = new Сar("Toyota ", 4, 150);
+            сar.Sprg();
+            Truck truck = new Truck("Volvo ", 6, 400, 10);
+            truck.Sprg();
+            сar.Brand("Mazda ");
+            truck.Gruz(12);
+            listBox1.Items.Add(сar.Sprg() + "\n\n");
+            listBox1.Items.Add(truck.Sprg() + "\n\n");
+        }
+    }
+}
